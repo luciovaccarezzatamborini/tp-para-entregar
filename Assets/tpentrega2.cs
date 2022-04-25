d3d6d2599178668a47f5d0c9ce63a7fd02f479c9
luciovaccarezzatamborini <101400339+luciovaccarezzatamborini@users.noreply.github.com>
2022-04-25 17:53:57 -0300

    final

--- Assets/tpentrega2.cs
+++ Assets/tpentrega2.cs
@@ -7,12 +7,16 @@ public class tpentrega2 : MonoBehaviour
 {
     string contraseñacorrecta;
     string contraseñausuario;
-    Text textoingresado;
+     public Text textoingresado;
+    public GameObject cartel_msj;
+    public Text textomsj;
 
     // Start is called before the first frame update
     void Start()
     {
         contraseñacorrecta = "12345";
+        cartel_msj.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -26,10 +30,14 @@ public class tpentrega2 : MonoBehaviour
 
         if(contraseñausuario == contraseñacorrecta)
         {
+            cartel_msj.SetActive(true);
+            textomsj.text = "Bienvenido";
             Debug.Log("Bienvenido");
         }
         else
         {
+            cartel_msj.SetActive(true);
+            textomsj.text = "Contraseña Incorrecta";
             Debug.Log("Contraseña incorrecta");
         }
     }
