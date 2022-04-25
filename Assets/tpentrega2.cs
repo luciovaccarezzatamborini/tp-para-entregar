7008d5623365c58a6eb57b4cdce5b8859b6e912d
luciovaccarezzatamborini <101400339+luciovaccarezzatamborini@users.noreply.github.com>
2022-04-24 23:43:20 -0300

    Initial commit

---
+++ Assets/tpentrega2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tpentrega2 : MonoBehaviour
+{
+    string contraseñacorrecta;
+    string contraseñausuario;
+    Text textoingresado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñacorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void validarcontra()
+    {
+        contraseñausuario = textoingresado.text;
+
+        if(contraseñausuario == contraseñacorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# tp-para-entregar
+ 
